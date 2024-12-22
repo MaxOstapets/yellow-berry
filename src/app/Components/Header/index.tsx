236ea72d6d7c0b7513469fe4213a759b71deec85
Max Ostapets <maksostapets2007@gmail.com>
2024-12-22 18:22:20 +0200

    need to fix hydration error

---
+++ src/app/Components/Header/index.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+const Header = () => {
+    return (
+        <header>
+            <div className="font-serif font-light text-sm leading-7 tracking-tightest text-white flex justify-around items-center bg-dark-gray">
+                <span>Flat 50% Off On Grocery Shop.</span>
+                <ul>
+                    <li>Help?</li>
+                    <li>Track Order</li>
+                    <li>Language</li>
+                    <li>Currency</li>
+                </ul>
+            </div>
+        </header>
+    )
+}
+
+export default Header
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,30 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'dark-blue': '#6C7FD8',
+      'purple': '#8118D5',
+      'light-blue': '#5F6AF5',
+      'dark-beige': '#F5885F',
+      'birch': '#32DBE2',
+      'very-dark-blue': '#3F51B5',
+      'red': '#F44336',
+      'pink': '#E91E63',
+      'gray': '#607D8B',
+      'dark-gray': '#3D4750',
+      'green': '#5EB595',
+      'white': '#FFFFFF',
+    },
+    
+    letterSpacing: {
+      'tightest': '0.48px'
+    },
+
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      fontFamily: {
+        serif: ['Poppins', 'serif'],
+        sans: ['Quicksand', 'sans-serif']
+      }
     },
   },
   plugins: [],
